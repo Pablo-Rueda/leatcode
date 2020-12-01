f77c08141e5c3b7e7784e1fca03d3e313916b89a
pablo-rueda <pablors-j@hotmail.com>
2020-12-01 12:16:26 +0100

    problem 0026

---
+++ leetcode0026.js
@@ -0,0 +1,55 @@
+/* Problem 0026: Merge Two Sorted Lists
+
+Given a sorted array nums, remove the duplicates in-place such that each element 
+appears only once and returns the new length.
+
+Do not allocate extra space for another array, you must do this by modifying the input 
+array in-place with O(1) extra memory.
+*/
+
+// Solution:
+//------------
+
+
+/*
+function removeDuplicates(nums){
+    for(i itarete over elements){
+        if (i is an index of array - i){
+            remove i;
+        }
+    }
+    return nums
+}
+*/
+
+
+var removeDuplicates = function(nums) {
+    let lgth = nums.length;
+    let i = 0;
+    //for(let i=0;i<lgth;i++)
+    while(i<lgth){
+        if(nums.slice(i+1,nums.length).indexOf(nums[i])>=0){
+            nums.splice(i,1)
+            lgth -= 1;
+        }else{
+            i++;
+        }
+    }
+    return nums.length;
+};
+
+
+// leetcodes faster solution:
+var removeDuplicates = function(nums) {
+    var idx = 0, i = 1;
+    while (i <= nums.length) {
+        if (nums[i] !== nums[idx]) {
+            nums[++idx] = nums[i];
+        }
+        i++;
+    }
+    return idx;
+};
+
+let input = [0,0,1,1,1,2,2,3,3,4]
+removeDuplicates(input)
